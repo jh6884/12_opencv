4735d135ded642b21024a1cc7b9f81f2811be62c
shinjaehyeon <pumpkinalpha996@gmail.com>
2025-08-12 10:38:26 +0900

    update code, .gitignore

--- src/helloworld.py
+++ src/helloworld.py
@@ -2,6 +2,16 @@ from ultralytics import YOLO
 
 model = YOLO('yolo11n.pt')
 
-results = model('https://ultralytics.com/images/bus.jpg')
+# results = model('https://ultralytics.com/images/bus.jpg')
+
+test_images = [
+    'https://ultralytics.com/images/bus.jpg',
+    'https://ultralytics.com/images/zidan.jpg',
+
+]
+
+for img in test_images:
+    results = model(img)
+    print(f'검출된 객체 수 : {len(results[0].boxes)}')
 
 results[0].show()
